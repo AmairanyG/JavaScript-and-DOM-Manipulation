93791ef7005a010c871cb9110613438b65bd3fc3
Amairani Garcia <amairany.gg@gmail.com>
2019-10-15 16:12:07 -0700

    updating code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,42 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// Function to build table
+function buildTable(data){
+    // Clear out existing data
+    tbody.html("");
+    data.forEach(dataRow => {
+        console.table(dataRow);
+        let row = tbody.append("tr");
+       // Append table data 
+       console.table(Object.values(dataRow));
+       Object.values(dataRow).forEach((val) => {
+           let cell = row.append("td");
+           cell.text(val);
+       });
+    });
+};
+
+// Event that calls a function named handleClick
+function handleClick(){
+    d3.event.preventDefault() // Event to prevent the form from refreshing the page
+    
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
+
+    // If date is entered, filter the data using that date, so only rows matching the filter value are displayed
+    if (date){
+        filterData = filterData.filter((row) => row.datetime === date);
+    };
+    
+    // Execute build table function, so data may be filtered when a date is typed
+    buildTable(filterData);
+};
+
+// Execute handleClick function when filter button is clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Execute funtion to display table data
+buildTable(tableData);
